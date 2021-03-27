f1e01c103d0770136af44c0dd47173729796b16f
Ziad Akram Bathish <37141372+ziadba95@users.noreply.github.com>
2021-03-27 14:26:56 +0100

    Few changes to the layout

--- BPRCoronaFighter/Controllers/HomeController.cs
+++ BPRCoronaFighter/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace BPRCoronaFighter.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About us";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact us";
 
             return View();
         }
