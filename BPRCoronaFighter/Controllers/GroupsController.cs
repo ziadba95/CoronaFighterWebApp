6ba970edb91302b6c00c403b6b06e5cb34e83d75
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2021-03-30 16:50:08 +0200

    group and lecture view fixed

---
+++ BPRCoronaFighter/Controllers/GroupsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BPRCoronaFighter.Models;
+
+namespace BPRCoronaFighter.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private BPRCoronaFighterContext db = new BPRCoronaFighterContext();
+
+        // GET: Groups
+        public ActionResult Index()
+        {
+            //return View(db.Groups.ToList());
+            return View();
+        }
+
+        // GET: Groups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // GET: Groups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Groups/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GroupId,GroupName")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Groups.Add(group);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(group);
+        }
+
+        // GET: Groups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: Groups/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GroupId,GroupName")] Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(group).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(group);
+        }
+
+        // GET: Groups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        // POST: Groups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Group group = db.Groups.Find(id);
+            db.Groups.Remove(group);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
