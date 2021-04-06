8fcc167fb48d1c0bd53a15c5596e142ad8fc2e5b
Angel Petrov <33376214+Angel266333@users.noreply.github.com>
2021-04-06 17:22:15 +0200

    Work on video section

---
+++ BPRCoronaFighter/Models/Video.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CompareAttribute = System.ComponentModel.DataAnnotations.CompareAttribute;
+
+namespace BPRCoronaFighter.Models
+{
+	public class Video
+	{
+		[HiddenInput(DisplayValue = false)]
+		public int? VideoId { get; set; }
+
+		[Required]
+		[DisplayName("Video Title")]
+		public string VideoTitle { get; set; }
+
+		[Required]
+		[DisplayName("Video URL")]
+		public string VideoURL { get; set; }
+
+		[Required]
+		[DisplayName("Image Link")]
+		public string ImageLink { get; set; }
+
+	}
+}
---
+++ DataLibrary/BusinessLogic/VideoProcessor.cs
@@ -0,0 +1,33 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+	class VideoProcessor
+	{
+        public static int AddVideo(string vtitle, string vURL, string imgLink)
+        {
+            VideoModel data = new VideoModel
+            {
+                VideoTitle = vtitle,
+                VideoURL = vURL,
+                ImageLink = imgLink,
+             
+            };
+            string sql = @"insert into dbo.[Video] (videoTitle, videoUrl, imageLink)
+                                  values(@VideoTitle, @VideoURL, @ImageLink);";
+            return DAO.SaveData(sql, data);
+        }
+        public static List<VideoModel> LoadVideos()
+        {
+            string sql = @"select  videoTitle, videoUrl, imageLink
+                              from dbo.[User];";
+            return DAO.LoadData<VideoModel>(sql);
+        }
+    }
+}
