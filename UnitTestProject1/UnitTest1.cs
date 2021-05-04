d5daa98644daaee96fafd696706d64c6f7d8b360
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2021-05-04 15:41:40 +0200

    0

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using BPRCoronaFighter.Controllers;
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// UnitTest2 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
---
+++ UnitTestProject3/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using BPRCoronaFighter.Controllers;
+namespace UnitTestProject3
+{
+    /// <summary>
+    /// UnitTest2 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class UnitTest2
+    {
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }//原文出自【易百教程】，商业转载请联系作者获得授权，非商业请保留原文链接：https://www.yiibai.com/asp.net_mvc/asp.net_mvc_unit_testing.html
+
+
+    }
+}
