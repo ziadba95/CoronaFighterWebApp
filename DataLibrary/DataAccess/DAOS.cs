38d91479476d8b9359af48261c0d59e7d1b76208
Chunhui Liu <37141993+ChunhuiL@users.noreply.github.com>
2021-05-29 21:52:09 +0200

    0529

---
+++ DataLibrary/DataAccess/DAOS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.DataAccess
+{
+    public interface DAOS
+    {
+         List<T> LoadData<T>(string sql);
+
+         List<T> LoadData<T>(string sql, T data);
+
+         int SaveData<T>(string sql, T data);
+
+         void DeleteData(string sql);
+
+         bool GetData(string sql);
+
+         string GetDataString(string sql);
+
+         string GetUserName(string sql);
+       
+    }
+}
